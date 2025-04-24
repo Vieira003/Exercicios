524a0c0079d0385a3b5980583df8de581a94ce56
Vieira003 <miguel.v.pereira@aluno.senai.br>
2025-04-24 13:25:44 -0300

    Tarefa 24/04 pt 2

--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package main
 import "fmt"
 
 func main() {
-    // Criando um slice de 5 inteiros
+
     nums := []int{1, 2, 3, 4, 5}
 
-    // Adicionando mais 3 elementos
     nums = append(nums, 6, 7, 8)
 
-    // Imprimindo tudo
     fmt.Println("Slice completo:", nums)
-    fmt.Println("Tamanho (len):", len(nums))
-    fmt.Println("Capacidade (cap):", cap(nums))
+    fmt.Println("Tamanho len:", len(nums))
+    fmt.Println("Capacidade cap:", cap(nums))
 	
+    nomes := []string{"Alice", "Bruno", "Carlos", "Daniela", "Eduardo"}
+
+    fmt.Println("Dois primeiros:", nomes[:2])
+
+    fmt.Println("Dois últimos:", nomes[len(nomes)-2:])
+
+    fmt.Println("Nome do meio:", nomes[2:3])
 }

a387209cc9ad53537f8b79da264b18720f3396ff
Vieira003 <miguel.v.pereira@aluno.senai.br>
2025-04-24 14:16:27 -0300

    Tarefa 24/04 pt 5

--- main.go
+++ main.go
@@ -1,24 +1,18 @@
 package main
 
-import (
-	"fmt"
-)
-
-func analisarNotas(nota1, nota2 float64) (float64, string) {
-	media := (nota1 + nota2) / 2
-
-	if media >= 6 {
-		return media, "Aprovado"
-	} else {
-		return media, "Reprovado"
-	}
-}
+import "fmt"
 
 func main() {
+    capitais := map[string]string{
+        "SP" : "São Paulo",
+        "RJ" : "Rio de Janeiro",
+        "ES" : "Espírito Santo",
+    }
 
-	nota1 := 5.5
-	nota2 := 2.0
+    capitais["BH"] = "Belo Horizonte"
 
-	media, situacao := analisarNotas(nota1, nota2)
-	fmt.Printf("Média: %.2f\nSituação: %s\n", media, situacao)
+    for k,v := range capitais {
+        fmt.Println("Sigla, Nome", k, v)
+    }
+
 }
